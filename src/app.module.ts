83581668ea60c0f0fcceb29e55f6983caa4b0e04
Mateusz Myrcik <mateuszmyrcik@MacBook-Pro-Mateusz.local>
2022-12-28 17:52:51 +0100

    feat: connect expenses endpoint with mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExpensesModule } from './expenses/expenses.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const DB_PASSWORD = process.env.DB_PASSWORD; // TODO: adjust to production
 
 @Module({
-  imports: [ExpensesModule],
+  imports: [
+    ExpensesModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://mati:${DB_PASSWORD}@cluster0.gs8zv.mongodb.net/family_budget?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { DeleteResult, UpdateResult } from 'mongodb';
+import { Model } from 'mongoose';
+import { UniqueID } from 'src/domain-types/shared';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { Expense, ExpenseDocument } from './schemas/expense.schema';
 
 @Injectable()
 export class ExpensesService {
-  create(createExpenseDto: CreateExpenseDto) {
-    return 'This action adds a new expense';
+  constructor(
+    @InjectModel(Expense.name) private expenseModel: Model<ExpenseDocument>,
+  ) {}
+
+  async create(createCatDto: CreateExpenseDto): Promise<Expense> {
+    const createdExpense = new this.expenseModel(createCatDto);
+    return createdExpense.save();
   }
 
-  findAll() {
-    return `This action returns all expenses`;
+  async findAll(): Promise<Expense[]> {
+    return this.expenseModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} expense`;
+  async findOne(id: UniqueID): Promise<Expense> {
+    return this.expenseModel.findOne({ _id: id });
   }
 
-  update(id: number, updateExpenseDto: UpdateExpenseDto) {
-    return `This action updates a #${id} expense`;
+  async update(
+    id: UniqueID,
+    updateExpenseDto: UpdateExpenseDto,
+  ): Promise<UpdateResult> {
+    return this.expenseModel.updateOne(updateExpenseDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} expense`;
+  async remove(id: UniqueID): Promise<DeleteResult> {
+    return this.expenseModel.deleteOne({ _id: id });
   }
 }
